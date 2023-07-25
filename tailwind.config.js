7e9af3d0f3cf4fc1f75659143a2763862c849b6d
Sazzad <hmsazzadur420@gmail.com>
2023-07-26 04:30:53 +0600

    added nav

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["*"],
+    theme: {
+      extend: {
+        remove: {
+          backgroundColor: 'transparent',
+        },
+      },
+    },
+    plugins: [],
+  }
